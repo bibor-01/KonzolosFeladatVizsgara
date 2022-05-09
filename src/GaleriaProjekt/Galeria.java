515add6629cc8c200506f42e2552d495840b4b02
bibor-01 <balogh01biborka@gmail.com>
2022-05-09 15:55:35 +0200

    Main metodusban peldanyositas, funkciok bemutatasa,cim, keszito bejerasa, megjelenito metodus bemutatasa, kep.txt letrehozasa

--- src/GaleriaProjekt/Galeria.java
+++ src/GaleriaProjekt/Galeria.java
@@ -1,11 +1,16 @@
-
 package GaleriaProjekt;
 
+import java.io.File;
+import java.lang.System.Logger;
+import java.lang.System.Logger.Level;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-public class Galeria {
+public class Galeria implements Iterable<KiallitasiTargy>{
+
     private ArrayList<KiallitasiTargy> targyak;
 
     public Galeria(ArrayList<KiallitasiTargy> targyak) {
@@ -13,17 +18,55 @@ public class Galeria {
     }
 
     public Galeria() {
+        targyak = new ArrayList<>();
+        try {
+            //készítő és cím
+            Festmeny f1 = new Festmeny("keszito1", "cim3");
+            //készítő és cím a mai dátummal
+            Festmeny f2 = new Festmeny(LocalDate.now(), "keszito3", "cim2");
+            //készítő és cím a hibás dátummal
+            Festmeny f3 = new Festmeny(LocalDate.parse("2022-11.14."), "keszito2", "cim1");
+            //készítő és cím fájl eléréssel
+            Festmeny f4 = new Festmeny(new File("kep.txt"), "keszito5", "cim4");
+            //készítő és cím fájl eléréssel, mai dátummal
+            Festmeny f5 = new Festmeny(new File("kep.txt"), LocalDate.now(), "keszito4", "cim5");
+            targyak.add(f1);
+            targyak.add(f2);
+            targyak.add(f3);
+            targyak.add(f4);
+            targyak.add(f5);
+        } catch (HibasDatumException ex) {
+            java.util.logging.Logger.getLogger(Galeria.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
     }
     
-    
-    public List<KiallitasiTargy> rendezCimSzerint(){
+    public void felvesz(KiallitasiTargy targy){
+        targyak.add(targy);
+    }
+
+    public List<KiallitasiTargy> rendezCimSzerint() {
         Collections.sort(targyak, KiallitasiTargy.cimRendezo());
         return Collections.unmodifiableList(targyak);
     }
-    
-    public List<KiallitasiTargy> rendezKeszitoSzerint(){
+
+    public List<KiallitasiTargy> rendezKeszitoSzerint() {
+        /*
+        unmodifiableList:
+            -a megadott lista nem módosítható nézetének megjelenítésére szolgál. 
+             Ha bármilyen kísérlet történik a visszaadott lista módosítására akár
+             közvetlenül, akár az iterátorán keresztül, az UnsupportedOperationException 
+             kivételt eredményez.
+        */
         Collections.sort(targyak, KiallitasiTargy.keszitoRendezo());
-        return Collections.unmodifiableList(targyak);
+
+        return Collections.unmodifiableList(targyak);//<-A galéria legyen bejárható, amikor visszaadja tárgyait, nem módosítható (vagy másolat) listában.
     }
-    
+
+    @Override
+    //Ez ahoz szükséges hogy forEach-el be tudjuk járni a galériát.A galéria legyen bejárható->eladat rész.
+    public Iterator<KiallitasiTargy> iterator() {
+        return Collections.unmodifiableList(targyak).iterator();
+    }
+
 }
--- src/GaleriaProjekt/Program.java
+++ src/GaleriaProjekt/Program.java
@@ -1,10 +1,30 @@
-
 package GaleriaProjekt;
 
 public class Program {
 
     public static void main(String[] args) {
-        
+        Galeria galeria = new Galeria();
+        System.out.println("cím szerint rendezés:");
+        for (KiallitasiTargy targy : galeria.rendezCimSzerint()) {
+            System.out.println(targy);
+        }
+
+        System.out.println("készítő szerint rendezés:");
+        for (KiallitasiTargy targy : galeria.rendezKeszitoSzerint()) {
+            System.out.println(targy);
+        }
+
+        //végül mutassuk be a megjelenítő metódust
+        for (KiallitasiTargy targy : galeria) {
+            /*
+            instanceof:
+                -összehasonlító operátorként is ismert, mivel összehasonlítja a példányt a típussal. Igazat vagy hamisat ad vissza. 
+                Ha bármilyen null értékű változóval alkalmazzuk az instanceof operátort, akkor false értéket ad vissza.*/
+            if (targy instanceof Festmeny) {
+                ((Festmeny) targy).megjelenites();
+            }
+
+        }
     }
-    
+
 }
